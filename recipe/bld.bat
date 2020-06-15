24b641a4c0fcaa1057dffae23b82e86af12f35b9
Tom Schoonjans <Tom.Schoonjans@rfi.ac.uk>
2020-06-15 13:36:32 +0100

    Use autotools_clang_conda

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,2 @@
-REM Create temp folder for bash
-bash -lc "ln -s ${LOCALAPPDATA}/Temp /tmp"
-
-bash -l %RECIPE_DIR%\build-windows.sh
-
+call %BUILD_PREFIX%\Library\bin\run_autotools_clang_conda_build.bat
+if errorlevel 1 exit 1
