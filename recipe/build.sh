93f6217f621d6b7741316fa4d4ef90b43b13b5b5
Tom Schoonjans <Tom.Schoonjans@genomicsplc.com>
2022-06-05 10:11:32 +0100

    Use meson/ninja for unix builds

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,37 @@
 set -e -x
 
-./configure --enable-python --prefix=$PREFIX
-make
-make install
+export CI=true
+
+if [[ $(uname) == Darwin ]]; then
+  export FCFLAGS="-isysroot $CONDA_BUILD_SYSROOT $FCFLAGS"
+  export PKG_CONFIG=$BUILD_PREFIX/bin/pkg-config
+fi
+
+if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" ]]; then
+  # Bash scripts with shebang lines calling bash scripts
+  # are not supported. Since the python on PATH when cross
+  # compiling is a bash script, this breaks calling cython
+  # workaround is to change the shebang lie of cython to call
+  # python bash script indirectly.
+  mv $BUILD_PREFIX/bin/cython $BUILD_PREFIX/bin/cython.bak
+  echo '#!/usr/bin/env python' > $BUILD_PREFIX/bin/cython
+  cat $BUILD_PREFIX/bin/cython.bak >> $BUILD_PREFIX/bin/cython
+fi
+
+if [ -z "$MESON_ARGS" ]; then
+  # for some reason this is not set on Linux
+  MESON_ARGS="--buildtype=release --prefix=${PREFIX} --libdir=lib"
+fi
+
+mkdir build
+cd build
+meson ${MESON_ARGS} \
+      --default-library=shared \
+      -Dbuild-documentation=false \
+      -Dpython=$PYTHON \
+      ..
+ninja
+if [[ "$CONDA_BUILD_CROSS_COMPILATION" != "1" ]]; then
+  ninja test
+fi
+ninja install
